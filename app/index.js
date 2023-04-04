7cf9937ecc91d1dff634462311968c897eb962f2
unknown <gippatrick@gmail.com>
2023-04-04 01:13:28 -0700

    Added image background functionality and added overlay

--- app/index.js
+++ app/index.js
@@ -1,18 +1,34 @@
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View, ImageBackground } from "react-native";
+import users from "../assets/data/users";
+
+const user = users[0];
 
 export default function Page() {
   return (
     <View style={styles.container}>
-      <View style={styles.userCard}>
+      <ImageBackground source={{ uri: user.coverImage }} style={styles.userCard}>
+        {/* OVERLAY FOR IMAGE BACKGROUND */}
+        <View style={styles.overlay} />
         {/* IMAGE */}
         <Image
-          src="https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png"
+          src={user.avatar}
           style={styles.userImage}
         />
         {/* NAME & HANDLE */}
-        <Text>Elon Musk</Text>
-        <Text>@elonmusk</Text>
-      </View>
+        <View>
+          <Text
+            style={{
+              color: 'white',
+              fontSize: 22,
+              fontWeight: '500',
+              marginBottom: 5,
+            }}
+          >
+            {user.name}
+          </Text>
+          <Text style={{ color: 'white' }}>@{user.handle}</Text>
+        </View>
+      </ImageBackground>
     </View>
   );
 }
@@ -23,11 +39,29 @@ const styles = StyleSheet.create({
     padding: 10,
     paddingTop: 75,
   },
+  overlay: {
+    backgroundColor: 'rgba(0, 0, 0, 0.2)',
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
   userCard: {
-    
+    backgroundColor: 'gray',
+    padding: 10,
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+
+    borderRadius: 10,
+    overflow: 'hidden',
   },
   userImage: {
     width: 100,
     height: 100,
+    borderRadius: 100,
+    borderColor: 'white',
+    borderWidth: 3.5,
+    marginRight: 20,
   },
 });
