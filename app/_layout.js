2274fef2a32633d07b62b15852936788ae8e8b13
unknown <gippatrick@gmail.com>
2023-04-08 16:05:23 -0700

    Implemented signup authentication with AWS and signout function

--- app/_layout.js
+++ app/_layout.js
@@ -1,10 +1,27 @@
 import { Stack } from "expo-router";
 import { Amplify } from "aws-amplify";
 import awsconfig from '../src/aws-exports';
+import { Authenticator } from '@aws-amplify/ui-react-native';
+import { KeyboardAvoidingView, Platform } from "react-native";
+
 
 Amplify.configure(awsconfig);
 
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios'
+        ? 'padding'
+        : 'height'
+      }
+      style={{ flex: 1 }}
+    >
+      <Authenticator.Provider>
+        <Authenticator>
+          <Stack screenOptions={{ headerShown: false }} />
+        </Authenticator>
+      </Authenticator.Provider>
+    </KeyboardAvoidingView>
+  )
 }
--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 // UTILITIES
-import { StyleSheet, View, FlatList, SafeAreaView } from "react-native";
+import { StyleSheet, View, FlatList, Text } from "react-native";
 import { Link } from "expo-router";
+import { useAuthenticator } from "@aws-amplify/ui-react-native";
 
 // Components
 import UserCard from "../src/components/UserCard";
@@ -13,12 +14,14 @@ import { Entypo } from '@expo/vector-icons';
 
 
 export default function Page() {
+  const { signOut } = useAuthenticator()
   return (
     <View style={styles.container}>
       <View style={styles.contentContainer}>
         <Link href={'/newPost'} style={styles.createNewPost}>
           <Entypo name="plus" size={15} color="white" />CREATE NEW POST
         </Link>
+        <Text onPress={() => signOut()}>Sign out</Text>
         <FlatList
           data={users}
           renderItem={({ item }) => <UserCard user={item} />}
